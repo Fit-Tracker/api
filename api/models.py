fa2c3e37ec6d62fcd33e4e130eb545d3e12da3be
Bob Amand <biotmg@nc.rr.com>
2015-10-27 08:19:26 -0400

    working draft database

--- api/models.py
+++ api/models.py
@@ -1,12 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+'''
+Consider a database table with ForeignKey.
+May want to set up something more flexible to add/delete new/old Activites
+'''
 #  from datetime import datetime
 
 # Create your models here.
+WALKING = 'Walking'
+RUNNING = 'RUNNING'
+CYCLING = 'Cycling'
+WEIGHTS = 'Weights'
+YOGA = 'Yoga'
+SWIMMING = 'Swimming'
+
+ACTIVITY_LIST = (
+    (WALKING, 'Walking'),
+    (RUNNING, 'Running'),
+    (CYCLING, 'Cycling'),
+    (WEIGHTS, 'Weights'),
+    (YOGA, 'Yoga'),
+    (SWIMMING, 'Swimming'),
+    )
 
 
 class Activity(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=10, choices=ACTIVITY_LIST)
     user = models.ForeignKey(User)   # one to many with Foreign Key
 
 

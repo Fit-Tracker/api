bee0e793cedb47c7e862f1e125acfeedf0ecd089
Bob Amand <biotmg@nc.rr.com>
2015-10-26 23:46:01 -0400

    initial commit with model mods

--- api/models.py
+++ api/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+#  from datetime import datetime
 
 # Create your models here.
 
 
 class Activity(models.Model):
     title = models.CharField(max_length=255)
-    user = models.ForeignKey(  # need something here )
+    user = models.ForeignKey(User)   # one to many with Foreign Key
 
 
 class Stat(models.Model):
-    timestamp=models.DateField(null=True)
-    stat=models.IntegerField()
-    activity=models.ForeignKey(Activity)
+    timestamp = models.DateField(null=True)
+    stat = models.IntegerField(default=0)
+    activity = models.ForeignKey(Activity)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(username=user.username,
+                                password=request.POST['password1'])
+            login(request, user)
+            return redirect('home_page', request.user.username)
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'api/register.html', {'form': form})

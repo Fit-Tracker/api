33d896b94716852b3e0bcf1c3b53c123b2a3bcaf
Ryan Burton <rryanburton@gmail.com>
2015-10-27 21:50:47 -0400

    added postgres, admin views

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Activity, Stat
 # Register your models here.
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ['user_id', 'title']
+
+
+class StatAdmin(admin.ModelAdmin):
+    list_display = ['activity_id', 'stat', 'timestamp']
+
+admin.site.register(Stat, StatAdmin)
+admin.site.register(Activity, ActivityAdmin)
--- fit_tracker/settings.py
+++ fit_tracker/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'fit_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fit_tracker',
+        'USER': 'fit_tracker',
+        'PASSWORD': 'password123',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
